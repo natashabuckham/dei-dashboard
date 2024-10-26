a16ae0d2cd603db153c9d68aeaa5e4d655ec41d2
Joshua <joshuaisaact@gmail.com>
2024-10-26 17:47:15 +0100

    feat: builds

--- lib/types.ts
+++ lib/types.ts
@@ -10,3 +10,15 @@ export interface SearchBarProps {
   minSearchLength?: number;
   debounceMs?: number;
 }
+
+export interface CompanyData {
+  EmployerName: string;
+  EmployerId: number;
+  Address: string;
+  DiffMeanHourlyPercent: number;
+  DiffMedianHourlyPercent: number;
+  CompanyLinkToGPGInfo: string;
+  EmployerSize: string;
+  CompanyName: string;
+  Year: string;
+}
