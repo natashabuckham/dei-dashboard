3356075e472338cb6571277d525a74cb53bd654b
Joshua <joshuaisaact@gmail.com>
2024-10-26 17:09:03 +0100

    feat: multiple category types

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import CategoryItem from "./components/CategoryItem";
 import SearchBar from "./components/ui/SearchBar";
 import { Providers } from "./components/Providers";
+import { diversities, industries } from "./constants/categories";
 
 export default function Home() {
-  const categories = ["Ethnicity", "Gender", "Disabilities", "LGBTQ"];
-
   return (
     <Providers>
       <div className="grid place-items-center min-h-screen p-8">
@@ -13,9 +12,26 @@ export default function Home() {
             DEI DASHBOARD
           </h1>
           <SearchBar />
-          {categories.map((category) => (
-            <CategoryItem key={category} category={category} />
-          ))}
+          <nav aria-label="Diversities">
+            <h2 className="sr-only">Diversities</h2>
+            {diversities.map((category) => (
+              <CategoryItem
+                key={category}
+                category={category}
+                className="bg-green-500 hover:bg-green-600"
+              />
+            ))}
+          </nav>
+          <nav aria-label="Industries">
+            <h2 className="sr-only">Industries</h2>
+            {industries.map((category) => (
+              <CategoryItem
+                key={category}
+                category={category}
+                className="bg-blue-500 hover:bg-blue-600"
+              />
+            ))}
+          </nav>
         </main>
       </div>
     </Providers>
